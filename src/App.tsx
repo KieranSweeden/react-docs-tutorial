2dae81ac975d213c004843175d730df6b3876ff5
Kieran <kieransweeden@gmail.com>
2023-04-07 14:52:24 +0100

    Add move history

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function calculateWinner(squares: number[]) {
+type SquareValue = "X" | "O" | null;
+
+function calculateWinner(squares: SquareValue[]) {
   const potentiallyWinnableLines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,6 +18,7 @@ function calculateWinner(squares: number[]) {
   ];
   for (const line of potentiallyWinnableLines) {
     const [a, b, c] = line;
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
@@ -23,7 +26,7 @@ function calculateWinner(squares: number[]) {
   return null;
 }
 
-const Square: FC<{ value: string; onSquareClick: MouseEventHandler }> = ({
+const Square: FC<{ value: SquareValue; onSquareClick: MouseEventHandler }> = ({
   value,
   onSquareClick,
 }) => {
@@ -34,16 +37,16 @@ const Square: FC<{ value: string; onSquareClick: MouseEventHandler }> = ({
   );
 };
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+const Board: FC<{
+  xIsNext: boolean;
+  squares: SquareValue[];
+  onPlay: Function;
+}> = ({ xIsNext, squares, onPlay }) => {
   function handleClick(i: number) {
-    if (squares[i] || calculateWinner(squares)) return;
+    if (calculateWinner(squares) || squares[i]) return;
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -71,13 +74,22 @@ function Board() {
       </div>
     </>
   );
-}
+};
 
 function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history.at(-1) as SquareValue[];
+
+  function handlePlay(nextSquares: SquareValue[]) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
